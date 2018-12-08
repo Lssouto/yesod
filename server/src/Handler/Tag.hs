c9e3e8ad3d3d3597c930bf6cb1bf7cb288191fb1
Bruna Russo <bruna.russo16@gmail.com>
2018-12-08 18:56:24 +0000

    Add getTagInMateria

--- server/src/Handler/Tag.hs
+++ server/src/Handler/Tag.hs
@@ -14,13 +14,16 @@ import Import
 --     runDB $ insert usuario
 --     sendStatusJSON ok200 (object ["resp" .= usuario])
     
--- getOneUsuarioR :: UserId -> Handler TypedContent
--- getOneUsuarioR userId = do
---     usuario <- runDB $ get404 userId
---     sendStatusJSON ok200 (object ["data" .= usuario])
     
 getTagR :: Handler TypedContent
 getTagR = do
     tag <- runDB $ selectList [] [Asc TagId]
     sendStatusJSON ok200 (object ["data" .= tag,
                                   "status" .= True ])
+                                  
+getTagInMateriaR :: MateriaId -> Handler TypedContent
+getTagInMateriaR materiaId = do
+    tag <- runDB $ selectList [TagIdMateria ==. materiaId] [Asc TagNome]
+    sendStatusJSON ok200 (object [ "data" .= tag
+                                 , "status" .= True
+                                 ])
