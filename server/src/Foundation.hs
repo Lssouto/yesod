198cacbd05414505ae566bf55f331a6b551ce18b
LSSouto <lssouto96@gmail.com>
2018-12-11 23:10:48 +0000

    adicionado token

--- server/src/Foundation.hs
+++ server/src/Foundation.hs
@@ -4,12 +4,17 @@
 {-# LANGUAGE MultiParamTypeClasses #-}
 {-# LANGUAGE TypeFamilies #-}
 {-# LANGUAGE ViewPatterns #-}
+{-# LANGUAGE LambdaCase #-}
 
 module Foundation where
 
 import Import.NoFoundation
 import Database.Persist.Sql (ConnectionPool, runSqlPool)
 import Yesod.Core.Types     (Logger)
+import qualified Web.JWT    as JWT
+import qualified Data.Map   as M
+import Data.Aeson (Result(..), fromJSON)
+
 
 data App = App
     { appSettings    :: AppSettings
@@ -21,8 +26,38 @@ data App = App
 
 mkYesodData "App" $(parseRoutesFile "config/routes")
 
+segredo :: Text
+segredo = "segredoAqui"
+
+chave :: Text
+chave = "chaveDoJsonAqui"
+
+untokenize :: Text -> Maybe Value
+untokenize t = (\x -> chave `M.lookup` JWT.unregisteredClaims (JWT.claims x))
+    =<< JWT.decodeAndVerifySignature (JWT.secret segredo) t
+
+tokenize :: UserId -> Text
+tokenize u = JWT.encodeSigned JWT.HS256 (JWT.secret segredo)
+    def {JWT.unregisteredClaims = M.fromList [(chave, toJSON u)]}
+
 instance Yesod App where
     makeLogger = return . appLogger
+
+    isAuthorized AuthenticateR _ = return Authorized
+    isAuthorized _     _ =  maybeAuthId >>= return . \case
+        Nothing -> Unauthorized "Precisa logar"
+        _       -> Authorized
+        
+        
+instance YesodAuth App where
+    type AuthId App = UserId
+
+    maybeAuthId = return . join . fmap
+        (fmap ((\(Success x) -> x) . fromJSON) . untokenize . decodeUtf8)
+        =<< lookupHeader "Authorization"
+
+-- instance Yesod App where
+--    makeLogger = return . appLogger
 
 instance YesodPersist App where
     type YesodPersistBackend App = SqlBackend
