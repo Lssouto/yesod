d7001eab424c08e3006383d01575f9770aba2ad8
LSSouto <lssouto96@gmail.com>
2018-12-12 00:10:13 +0000

    Adicionado model rota e handler para os eventos

---
+++ server/src/Handler/Evento.hs
@@ -0,0 +1,35 @@
+{-# LANGUAGE NoImplicitPrelude #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE LambdaCase #-}
+
+module Handler.Evento where
+
+import Import
+import Network.HTTP.Types.Status
+import Database.Persist.Postgresql
+import Prelude (read)
+
+getEventoR :: Handler TypedContent
+getEventoR = do
+    usrid <- maybeAuthId
+    case usrid of
+        Just uid -> do 
+                    evento <- runDB $ selectList [ EventoIdUser ==. uid ] [Asc EventoTitulo]
+                    sendStatusJSON ok200 (object [ "data" .= evento
+                                                 , "status" .= True 
+                                                 ])
+        _ -> sendStatusJSON ok200 (object ["status" .= False])
+    
+                                 
+postEventoR :: Handler TypedContent
+postEventoR = do
+    evento <- requireJsonBody :: Handler Evento
+    usrid <- maybeAuthId
+    case usrid of
+        Just uid -> do 
+                    runDB . insert $ evento { eventoIdUser = uid }
+                    sendStatusJSON ok200 (object ["status" .= True])
+        _ -> sendStatusJSON ok200 (object ["status" .= False])
